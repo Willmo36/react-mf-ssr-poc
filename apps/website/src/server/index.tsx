ae4dc18901466cdeddc3341344bbb48a4358aabb
Max Willmott <max.willmott@justeattakeaway.com>
2022-11-23 14:31:19 -0700

    Initial server build

--- apps/website/src/server/index.tsx
+++ apps/website/src/server/index.tsx
@@ -1,17 +1,21 @@
 import express from "express";
 import React from "react";
 import { fragmentHandler } from "server-shared";
-import webpack from "webpack";
-import webpackDevMiddleware from "webpack-dev-middleware";
 import { Html } from "../components/Html";
-const webpackConfig = require("../../webpack.config.js");
 
-const compiler = webpack(webpackConfig);
 const app = express();
 const port = process.env.WEBSITE_PORT ?? "No port passed";
 
+console.log("the env pleaze", process.env.NODE_ENV);
+if (process.env.NODE_ENV === 'development') {
+  const webpack = require("webpack");
+  const webpackDevMiddleware = require("webpack-dev-middleware");
+  const webpackConfig = require("../../webpack.config.js");
+  const compiler = webpack(webpackConfig);
+  app.use("/js/", webpackDevMiddleware(compiler));
+}
+
 app.use(express.static("public"));
-app.use("/js/", webpackDevMiddleware(compiler));
 
 app.get(
   "/",
